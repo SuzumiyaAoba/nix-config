17138956f772754c27815413794f102ac409f90f
SuzumiyaAoba <SuzumiyaAoba@gmail.com>
2023-12-17 16:04:13 +0900

    feat: add git alias

--- home/base/commands/git/default.nix
+++ home/base/commands/git/default.nix
@@ -4,6 +4,10 @@
   programs.git = {
     enable = true;
 
+    aliases = {
+      tree = "log --graph --pretty=format:'%x09%C(auto) %h %Cgreen %ar %Creset%x09by %C(cyan ul)%an%Creset %x09%C(auto)%s %d'";
+    };
+
     ignores = [
       "*~"
       "*.swp"
