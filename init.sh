604dd0607ed927ba4e035f6ea35336a473cffc44
SuzumiyaAoba <SuzumiyaAoba@gmail.com>
2024-01-20 19:56:25 +0900

    fix(nix): init.sh

--- init.sh
+++ init.sh
@@ -3,8 +3,12 @@
 # Determinate Nix Installer
 curl --proto '=https' --tlsv1.2 -sSf -L https://install.determinate.systems/nix | sh -s -- install
 
+. /nix/var/nix/profiles/default/etc/profile.d/nix-daemon.sh 
+
 # Clone nix-config repository
 nix run nixpkgs#git -- clone https://github.com/SuzumiyaAoba/nix-config.git
+
+cd nix-config
 
 # Install with cargo-mamke
 nix --experimental-features 'nix-command flakes' shell nixpkgs#cargo-make -c makers 'private-aarch64:apply'
