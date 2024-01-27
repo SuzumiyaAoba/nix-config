96de288a0db4c598a2cd14c0b138b8a69bb13e13
SuzumiyaAoba <SuzumiyaAoba@gmail.com>
2024-01-27 22:04:50 +0900

    fix(emacs): change file coding

--- home/base/editor/emacs/init.el
+++ home/base/editor/emacs/init.el
@@ -63,7 +63,7 @@
 
   (set-language-environment "Japanese")
   (prefer-coding-system 'utf-8)
-  (set-default 'buffer-file-coding-system 'utf-8-with-signature)
+  (set-default 'buffer-file-coding-system 'utf-8)
 
   (setq global-auto-revert-non-file-buffers t)
   (global-auto-revert-mode)
