8991399830317c0a2507fdcd2357e6e6d5b31023
SuzumiyaAoba <SuzumiyaAoba@gmail.com>
2024-03-07 02:47:07 +0900

    feat(zsh): change word-chars

--- home/base/shell/zsh/config/init.zsh
+++ home/base/shell/zsh/config/init.zsh
@@ -2,6 +2,11 @@ setopt no_beep
 
 export LANG=ja_JP.UTF-8
 
+autoload -Uz select-word-style
+select-word-style default 
+zstyle ':zle:*' word-chars ' /=;@:{}[]()<>,|.'
+zstyle ':zle:*' word-style unspecified
+
 source "$HOME/.config/zsh/plugins.zsh"
 source "$HOME/.config/zsh/alias.zsh"
 source "$HOME/.config/zsh/keybinds.zsh"
