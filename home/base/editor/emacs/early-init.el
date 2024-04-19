b3e1578a554bc9904a61049117f2285edb23e5a2
SuzumiyaAoba <SuzumiyaAoba@gmail.com>
2024-04-20 04:28:09 +0900

    fix(emacs): change font timing

--- home/base/editor/emacs/early-init.el
+++ home/base/editor/emacs/early-init.el
@@ -12,13 +12,6 @@
 
 (set-face-attribute 'default nil :family "0xProto")
 
-(add-hook 'after-init-hook
-          (lambda ()
-            (cond ((display-graphic-p)
-                   (set-fontset-font nil 'japanese-jisx0213.2004-1 "HackGen Console NF")
-                   (set-fontset-font nil 'japanese-jisx0213-2 "HackGen Console NF")
-                   (set-fontset-font nil 'katakana-jisx0201 "HackGen Console NF")))))
-
 ;; lsp
 ;; see: https://emacs-lsp.github.io/lsp-mode/page/performance/#use-plists-for-deserialization
 (setenv "LSP_USE_PLISTS" "true")
