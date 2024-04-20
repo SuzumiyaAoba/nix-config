b7a9fc153a4e6096f3cb027d58c19c419fa7a369
SuzumiyaAoba <SuzumiyaAoba@gmail.com>
2024-04-20 19:43:21 +0900

    feat(zsh): check sdk command existance

--- home/base/shell/zsh/config/programs/sdkman.zsh
+++ home/base/shell/zsh/config/programs/sdkman.zsh
@@ -1,2 +1,4 @@
-export SDKMAN_DIR=$(brew --prefix sdkman-cli)/libexec
-[[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]] && source "${SDKMAN_DIR}/bin/sdkman-init.sh"
+if [[ $(command -v sdk) ]]; then
+    export SDKMAN_DIR=$(brew --prefix sdkman-cli)/libexec
+    [[ -s "${SDKMAN_DIR}/bin/sdkman-init.sh" ]] && source "${SDKMAN_DIR}/bin/sdkman-init.sh"
+fi
