5bfb50e38ac1a58b5af029d48d232e2eb9303138
SuzumiyaAoba <SuzumiyaAoba@gmail.com>
2024-06-15 18:36:03 +0900

    fix: org-modern configuration

--- home/base/editor/emacs/early-init.el
+++ home/base/editor/emacs/early-init.el
@@ -10,10 +10,6 @@
 ;; ツールバーを無効
 (push '(tool-bar-lines . 0) default-frame-alist)
 
-(push '(font . "0xProto-12") default-frame-alist)
-
-(set-face-attribute 'default nil :family "0xProto" :height 100)
-
 ;; lsp
 ;; see: https://emacs-lsp.github.io/lsp-mode/page/performance/#use-plists-for-deserialization
 (setenv "LSP_USE_PLISTS" "true")
