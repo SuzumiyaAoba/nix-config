32caaabe0c1932e39efa28d5254dcf253a9ca1ac
SuzumiyaAoba <SuzumiyaAoba@gmail.com>
2025-01-09 20:29:54 +0900

    feat: add GNU global

--- hosts/macos/aarch64-256GB/home.nix
+++ hosts/macos/aarch64-256GB/home.nix
@@ -72,6 +72,7 @@ in
     "home/programs/commands/taskwarrior.nix"
     "home/programs/commands/bugwarrior.nix"
     "home/programs/commands/docker.nix"
+    "home/programs/commands/global.nix"
 
     # LLM
     "home/programs/commands/ollama.nix"
