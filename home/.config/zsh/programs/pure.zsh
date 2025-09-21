828ca9a00927f6739434854ad29a94ad3257c58f
SuzumiyaAoba <SuzumiyaAoba@gmail.com>
2025-09-21 13:45:40 +0900

    feat: pure

---
+++ home/.config/zsh/programs/pure.zsh
@@ -0,0 +1,8 @@
+autoload -U promptinit; promptinit
+
+zstyle :prompt:pure:git:stash show yes
+zstyle :prompt:pure:execution_time show yes
+zstyle :prompt:prompt:success show yes
+zstyle :prompt:error show yes
+
+prompt pure
