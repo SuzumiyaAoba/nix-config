4e34dcad42a8d96491c967584d3916538e7571f2
suzumiyaaoba <SuzumiyaAoba@gmail.com>
2025-12-03 20:47:04 +0900

    wip

--- home/.config/nvim/init.lua
+++ home/.config/nvim/init.lua
@@ -1,1 +1,35 @@
 require("config.lazy")
+
+-- OS のクリップボードと連携
+vim.opt.clipboard:append({ "unnamed", "unnamedplus" })
+
+-- タブ
+vim.o.expandtab = true
+vim.o.tabstop = 2
+vim.o.shiftwidth = 2
+
+-- 行番号
+vim.opt.number = true
+
+-- フォント
+vim.o.guifont = "Cascadia Next JP:h14"
+
+-- エンコーディング
+vim.opt.fileencoding = "utf-8"
+vim.opt.fileencodings = { "ucs-bom", "utf-8", "euc-jp", "cp932" }
+
+-- カーソル
+vim.opt.cursorline = true
+vim.opt.virtualedit = "onemore"
+
+-- インデント
+vim.opt.smartindent = true
+
+-- 視覚ベル
+vim.opt.visualbell = true
+
+-- 括弧
+vim.opt.showmatch = true
+
+-- コマンドライン補完
+vim.opt.wildmode = { "list", "longest" }
