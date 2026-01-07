f37e9bdd637a60850c5a3adbe51bcb2beb163a38
suzumiyaaoba <SuzumiyaAoba@gmail.com>
2026-01-07 21:59:04 +0900

    fix: wezterm

--- home/.config/wezterm/keybinds.lua
+++ home/.config/wezterm/keybinds.lua
@@ -9,5 +9,7 @@ return {
     { key = '-', mods = 'SUPER', action = act.DecreaseFontSize },
     { key = 'c', mods = 'SUPER', action = act.CopyTo 'Clipboard' },
     { key = 'v', mods = 'SUPER', action = act.PasteFrom 'Clipboard' },
+    -- for Emacs
+    { key = "_", mods = "CTRL|SHIFT", action = wezterm.action.DisableDefaultAssignment },
   }
 }
