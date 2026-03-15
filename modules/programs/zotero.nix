34d4969d9e0c06c0387fd682870b574b41f698ea
SuzumiyaAoba <SuzumiyaAoba@gmail.com>
2026-03-15 23:15:11 +0900

    fix: zotero

--- modules/programs/zotero.nix
+++ modules/programs/zotero.nix
@@ -1,12 +1,23 @@
-{ delib, pkgs, ... }:
+{
+  delib,
+  lib,
+  pkgs,
+  ...
+}:
 delib.module {
   name = "programs.zotero";
 
   options = delib.singleEnableOption false;
 
-  home.ifEnabled = {
+  home.ifEnabled = lib.mkIf (!pkgs.stdenv.isDarwin) {
     home.packages = with pkgs; [
       zotero
+    ];
+  };
+
+  darwin.ifEnabled = {
+    homebrew.casks = [
+      "zotero"
     ];
   };
 }
