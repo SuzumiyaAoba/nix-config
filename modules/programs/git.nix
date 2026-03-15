c569dc9147fd0558240bc43afbdf37eae054b604
SuzumiyaAoba <SuzumiyaAoba@gmail.com>
2026-03-15 17:01:20 +0900

    chore: update git config

--- modules/programs/git.nix
+++ modules/programs/git.nix
@@ -21,39 +21,41 @@ delib.module {
           ".mise.local.toml"
         ];
 
-        settings = let
-          deltaOptions = [
-            "core.pager=delta"
-            "interactive.diffFilter='delta --color-only'"
-            "delta.navigate=true"
-            "delta.dark=true"
-            "delta.line-numbers=true"
-            "delta.side-by-side=true"
-            "delta.hyperlinks=true"
-            "delta.hyperlinks-file-link-format='vscode://file/{path}:{line}'"
-          ];
-        in {
-          user = {
-            name = myconfig.constants.username;
-            email = myconfig.constants.useremail;
-          };
-          merge = {
-            conflictStyle = "zdiff3";
-          };
-          alias = {
-            delete-merged-branch = "!f () { git checkout $1; git branch --merged|egrep -v '\\*|develop|main'|xargs git branch -d; git fetch --prune; };f";
+        settings =
+          let
+            deltaOptions = [
+              "core.pager=delta"
+              "interactive.diffFilter='delta --color-only'"
+              "delta.navigate=true"
+              "delta.dark=true"
+              "delta.line-numbers=true"
+              "delta.side-by-side=true"
+              "delta.hyperlinks=true"
+              "delta.hyperlinks-file-link-format='vscode://file/{path}:{line}'"
+            ];
+          in
+          {
+            user = {
+              name = myconfig.constants.username;
+              email = myconfig.constants.useremail;
+            };
+            merge = {
+              conflictStyle = "zdiff3";
+            };
+            alias = {
+              delete-merged-branch = "!f () { git checkout $1; git branch --merged|egrep -v '\\*|develop|main'|xargs git branch -d; git fetch --prune; };f";
 
-            dlog  = "-c diff.external=difft log --ext-diff";
-            dl    = "-c diff.external=difft log --ext-diff";
-            dshow = "-c diff.external=difft show --ext-diff";
-            ds    = "-c diff.external=difft show --ext-diff";
-            ddiff = "-c diff.external=difft diff";
-            dft   = "-c diff.external=difft diff";
+              dlog = "-c diff.external=difft log --ext-diff";
+              dl = "-c diff.external=difft log --ext-diff";
+              dshow = "-c diff.external=difft show --ext-diff";
+              ds = "-c diff.external=difft show --ext-diff";
+              ddiff = "-c diff.external=difft diff";
+              dft = "-c diff.external=difft diff";
 
-            delta  = "-c ${lib.concatStringsSep " -c " deltaOptions} diff";
-            dblame = "-c ${lib.concatStringsSep " -c " deltaOptions} blame";
+              delta = "-c ${lib.concatStringsSep " -c " deltaOptions} diff";
+              dblame = "-c ${lib.concatStringsSep " -c " deltaOptions} blame";
+            };
           };
-        };
       };
 
       programs.diff-so-fancy = {
